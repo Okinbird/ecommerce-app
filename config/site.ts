5829a699dc073093316fe2bf8b0fc94d6d054a58
Okinbird <okinyinka@yahoo.co.uk>
2023-07-10 17:38:46 -0700

    add basic layout, title, description.

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Name",
-  description: "Description",
+  name: "Grandeur",
+  description: "Expertly designed goods for the workspace, home and travel.",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
