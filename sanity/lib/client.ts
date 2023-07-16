eda8149a18c7394963d871b1c08b86aa47222741
Okinbird <okinyinka@yahoo.co.uk>
2023-07-16 16:22:44 -0700

    add sanity token for the seeded data

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,6 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token:
+    "skbwpuyPoFCEEE1STKIMT0ZpyLG218qbuzx4s7zsjZKJRgcMjOoXutqjvqbEZvfi0zLOStWgZ6Aw1mheS7NpcEaa00aF3vwGE30I3Q2uwT8z3dKUwd1M6KRquDOXCWeyLZEc6dJvzMPTdQmPOSUidbDMst7TEMRvV8gOngqdkmFmSImGACTQ"
 })
