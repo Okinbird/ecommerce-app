59b771dcbbc0a047bd55c0dad168fa403f179cc8
Okinbird <okinyinka@yahoo.co.uk>
2023-07-16 19:19:55 -0700

    sort the product page by order

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,24 @@ import { ProductFilters } from "@/components/product-filters"
 import { ProductGrid } from "@/components/product-grid"
 import { ProductSort } from "@/components/product-sort"
 
-interface Props {}
+interface Props {
+  searchParams: {
+    date?: string
+    price?: string
+  }
+}
 
-export default async function Page() {
+export default async function Page({searchParams}: Props) {
+  const priceOrder = searchParams.price
+    ? `| order(price ${searchparams.price})`
+    : ""
+  const dateOrder =  searchParams.date
+    ? `| order(_createdAt ${searchparams.date})`
+    : ""
+  const order = `${priceOrder}${dateOrder}`
+  
   const products = await client.fetch<SanityProduct[]>(
-    groq`*[_type == "product"] {
+    groq`*[_type == "product" ] ${order} {
       _id,
       _createdAt,
       name,
